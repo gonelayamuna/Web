bdfd58dae35a5787df43e6b35de1b1ae6b71483d
gonelayamuna <gonelayamuna@gmail.com>
2017-11-13 12:32:59 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,17 @@
+import calendar
+print("Hello")
+a,b,c="yamuna","G",123
+print (a*2)
+print (b)
+print (c)
+
+print (calendar.month(2017,10))
+
+str=int(input("Enter input one"+ "  "))
+
+str1=int(input("Enter input two"+"  "));
+
+
+
+sum=str+str1
+print ("Output is",sum);
